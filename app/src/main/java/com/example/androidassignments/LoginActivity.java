a1ba0dce8342af0abea72ea3d2355ba69b123619
prometheus0329 <guneetsingh0329@gmail.com>
2024-05-26 13:27:55 -0400

    Login Done for part 2

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -1,15 +1,11 @@
 package com.example.androidassignments;
 
-import static androidx.constraintlayout.widget.Constraints.TAG;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,17 +36,15 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                String email = loginEmail.getText().toString();
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("DefaultEmail", email);
-                editor.apply();
+        loginButton.setOnClickListener(v -> {
+            String email = loginEmail.getText().toString();
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("DefaultEmail", email);
+            editor.apply();
 
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         });
     }
 
